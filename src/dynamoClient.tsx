98c3fd8fa34d81b9417cfd8accafbb97ac419e39
forezight <115670302+forezight@users.noreply.github.com>
2024-12-25 12:54:46 -0300

    Update dynamoClient.tsx

--- src/dynamoClient.tsx
+++ src/dynamoClient.tsx
@@ -3,8 +3,8 @@ import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 const dynamoClient = new DynamoDBClient({
   region: "us-east-1",
   credentials: {
-    accessKeyId: "AKIAYMUM5WNZVASZCHKH",
-    secretAccessKey: "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"
+    accessKeyId: "",
+    secretAccessKey: ""
   }
 });
 
